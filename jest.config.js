1c5b5efa3b7800cebbca5153aa2142d9597e3aa2
Pedro Miguel <pedromrap@gmail.com>
2023-04-30 11:48:38 -0300

    chore: config jest setup

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,11 @@
 module.exports = {
-  testEnvironment: 'jest-environment-jsdom',
+  testPathIgnorePatterns: ['/node_modules'],
+  testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['<rootDir>/.jest/setup-tests.js'],
   collectCoverage: true,
   moduleNameMapper: {
     '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/.jest/__mocks__/fileMock.js',
     '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
   },
+  collectCoverageFrom: ['src/**/*.tsx', '!src/**/*.spec.tsx'],
 }

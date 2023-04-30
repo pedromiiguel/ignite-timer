bd1243e2e422f5d29c229a31ba70cef5631fb6ef
Pedro Miguel <pedromrap@gmail.com>
2023-04-30 11:51:07 -0300

    style: add custom style scrollbar

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,6 +1,7 @@
 import { createGlobalStyle, css } from 'styled-components'
 
 export const GlobalStyle = createGlobalStyle`
+${({ theme }) => css`
   * {
     padding: 0;
     margin: 0;
@@ -8,22 +9,35 @@ export const GlobalStyle = createGlobalStyle`
   }
 
   :focus {
-    ${({ theme }) => css`
-      outline: 0;
-      box-shadow: 0 0 0 2px ${theme.colors['green-500']};
-    `}
+    outline: 0;
+    box-shadow: 0 0 0 2px ${theme.colors['green-500']};
   }
 
   body {
-    ${({ theme }) => css`
-      background-color: ${theme.colors['gray-900']};
-      color: ${theme.colors['gray-300']};
-    `}
+    background-color: ${theme.colors['gray-900']};
+    color: ${theme.colors['gray-300']};
+    -webkit-font-smoothing: antialiased;
   }
 
-  body, input, textarea, button {
+  body,
+  input,
+  textarea,
+  button {
     font-family: 'Roboto', sans-serif;
     font-weight: 400;
     font-size: 1rem;
   }
+
+  ::-webkit-scrollbar {
+    width: 8px;
+  }
+
+  ::-webkit-scrollbar-track {
+    background: none;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background-color: ${theme.colors['gray-700']};
+  }
+`}
 `
